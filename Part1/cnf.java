26ed03f2d2a12d6df8462d2e92a82db7a464fc76
markverrajr1701 <ma09verr@siena.edu>
2023-04-13 01:40:02 -0400

    added directories and cnf.java

---
+++ Part1/cnf.java
@@ -0,0 +1,25 @@
+package Part1;
+
+import java.io.PrintWriter;
+import java.io.FileReader;
+import java.util.TreeMap;
+import java.util.Queue;
+import java.util.regex.*;
+
+/**
+ * 
+ */
+public class cnf {
+    public static final char NOT = '~';
+    public static final char AND = '&';
+    public static final char OR = '|';
+    public static final char IMPLIES = '>';
+    public static final char BICONDITIONAL = '=';
+
+
+
+}
+
+class jointProps {
+
+}
